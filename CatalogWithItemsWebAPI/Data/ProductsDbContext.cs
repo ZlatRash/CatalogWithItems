193e6926a2f81325066a9f055eabe29c1b8d1b85
ZlatRash <109162627+ZlatRash@users.noreply.github.com>
2023-01-09 22:17:47 +0500

    Initial commit

---
+++ CatalogWithItemsWebAPI/Data/ProductsDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CatalogWithItemsData.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogWithItemsWebAPI.Data
+{
+    public class ProductsDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }    
+
+        public ProductsDbContext(DbContextOptions<ProductsDbContext> options): base(options)
+        {
+
+        }
+    }
+}
